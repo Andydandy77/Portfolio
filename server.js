7e272236cc933104e4985d8b0a5bcdf91ebc28a3
Dave <david.j.anderson94@gmail.com>
2018-12-12 21:05:09 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require(path.join(__dirname, "./routes/htmlRoutes"))(app);
+
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+
+module.exports = { app };
+
